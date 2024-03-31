580f6dd067359d113ce01c6fbc373a55ab2f9b6a
A-MIKs <amiks262@gmail.com>
2024-03-31 14:59:02 +0100

    first commit

---
+++ account/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+
+from . import views
+
+urlpatterns = [
+    path("register/", views.register, name="register"),
+    path("edit/", views.edit, name="edit"),
+    path("", include("django.contrib.auth.urls")),
+    path("", views.dashboard, name="dashboard"),
+    path("users/", views.user_list, name="user_list"),
+    path("users/follow/", views.user_follow, name="user_follow"),
+    path("users/<username>/", views.user_detail, name="user_detail"),
+]
